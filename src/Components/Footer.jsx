24f130d625599ccff4adff750241a89812f1d701
Fidal <fidal15perfect@gmail.com>
2021-09-28 11:47:18 +0530

    fully responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { FaTwitter } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import Twitter from "./img/bt.png";
+import Github from "./img/bg.png";
+import Instagram from "./img/bi.png";
+import Linkedin from "./img/bl.png";
+
 
 export default function Footer() {
     return (
@@ -11,25 +12,36 @@ export default function Footer() {
 
             <div className="footer_icons">
 
-<a href="https://www.instagram.com/fidjoke/"  target="_blank" rel="noreferrer" className="footer_i">
-  <FaInstagram  size={50}   style={{ color: 'black' ,width:"23px",height:"23px" }} />
+            <div className="foot_social_icons">
+
+<a href="https://www.instagram.com/fidjoke/"  target="_blank" rel="noreferrer">
+<img src={Instagram} alt="" className="home_i"/>
+  {/* <FaInstagram  size={70}   style={{ color: 'white' ,width:"28px",height:"28px" }} /> */}
   </a>
-  <a href="https://twitter.com/fidalmathew10"  target="_blank" rel="noreferrer" className="footer_i">
-  <FaTwitter size={50} style={{ color: 'black' ,width:"23px",height:"23px"}} />
+  <a href="https://twitter.com/fidalmathew10"  target="_blank" rel="noreferrer">
+  {/* <FaTwitter size={70} style={{ color: 'white' ,width:"28px",height:"28px"}} /> */}
+  <img src={Twitter} alt="" className="home_i"/>
   </a>
-  <a href="https://www.linkedin.com/in/fidal-mathew-82aba7200/"  target="_blank" rel="noreferrer" className="footer_i">
-  <FaLinkedin size={50} style={{ color: 'black' ,width:"23px",height:"23px"}} />
+  <a href="https://www.linkedin.com/in/fidal-mathew-82aba7200/"  target="_blank" rel="noreferrer">
+  {/* <FaLinkedin size={70} style={{ color: 'white' ,width:"28px",height:"28px"}} /> */}
+  <img src={Linkedin} alt="" className="home_i"/>
   </a>
-  <a href="https://github.com/FidalMathew"  target="_blank" rel="noreferrer" className="footer_i">
-  <FaGithub size={50} style={{ color: 'black' ,width:"23px",height:"23px"}} />
+  <a href="https://github.com/FidalMathew"  target="_blank" rel="noreferrer">
+  {/* <FaGithub size={70} style={{ color: 'white' ,width:"28px",height:"28px"}} /> */}
+  <img src={Github} alt="" className="home_i"/>
   </a>
   
-<span style={{position:"relative",left:"1%",fontSize:"18px"}}>| No Bootstrap components Used</span>
+
+
+
+</div>
+  
+<span className="bootstrap" style={{position:"relative",left:"1%",fontSize:"18px"}}>| No Bootstrap components Used</span>
 
 
 </div>
 
-            <span style={{position:"relative",left:"43%",fontSize:"18px"}}>Copyright &#169;2021 | Made by Fidal</span>
+            <div className="copyright" style={{textAlign:"center",width:"80%"}}>Copyright &#169;2021 | Made by Fidal</div>
 
             </div>
         </div>
